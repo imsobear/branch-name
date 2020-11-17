e9552e617aa53c7ca796eb26027c6f34ad865a8d
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-11-17 12:56:13 +0800

    chore: test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');
 async function run() {
   try {
     console.log('1111111', process.env.GITHUB_REF);
+    console.log(222, process.env['GITHUB_ENV']);
     core.exportVariable('BRANCH_NAME', process.env.GITHUB_REF.split('/').slice(2).join('/'));
   }
   catch (error) {
